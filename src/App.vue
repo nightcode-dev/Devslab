fa704b9bc820e29486359e84bee4e39600578ea7
nightcode-dev <prog.nightcode@gmail.com>
2023-07-07 17:54:22 +0430

    add fade in effect-create main home page box

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="">
     <div class="fixed top-0 h-min flex justify-between p-2 w-full z-50 bg-gray-200 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border-b border-gray-400">
       <div class="flex items-center p-4 z-50 ">
         <router-link active-class="text-main" to="/" class="Bpey text-xl mx-2">خانه</router-link>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,52 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+
+var fadeInElements = []
+
+const isElemVisible = (el) => {
+      var rect = el.getBoundingClientRect()
+      var elemTop = rect.top + 100 // 200 = buffer
+      var elemBottom = rect.bottom
+      return elemTop < window.innerHeight && elemBottom >= 0
+}
+
+const handleScroll = (evt) => {
+      for (var i = 0; i < fadeInElements.length; i++) {
+        var elem = fadeInElements[i]
+        if (isElemVisible(elem)) {
+          elem.style.opacity = '1'
+          elem.style.transform = 'scale(1)'
+          fadeInElements.splice(i, 1) // only allow it to run once
+        }
+      }
+}
+
+onMounted(()=>{
+  fadeInElements = Array.from(document.getElementsByClassName('fadein'))
+  document.addEventListener('scroll',handleScroll)
+  handleScroll()
+})
+</script>
 
 <template>
-  <div>
-    
+  <div class="">
+    <div class="flex items-center justify-center bg-slate-100">
+      <div class="w-full flex flex-col items-center justify-center p-16">
+        <h1 class="text-black STpey text-2xl "><b class="text-main Bpey">devslab</b> چیه؟</h1>
+        <p class="text-gray-600 Mes text-md text-center">
+          <b class="text-main Bpey">دِوزلب</b> یک پلتفرم ایرانیه که یک محیط خیلی خوب برای نگه داری
+          ترتیب بندی و حتی ویرایش و نوشتن کد ها فراهم می کنه این محیط کاملا مناسب تیم ها طراحی شده
+          تا بتونن پروژه هاشون رو خیلی سریعتر توسعه بدن
+        </p>
+        <router-link to="/signup" class="fadein py-2 px-4 rounded-md border-2 border-main text-main hover:bg-main STpey text-lg hover:text-white mt-4">
+          شروع کن
+        </router-link>
+      </div>
+      <div class="w-full flex items-center justify-center p-16">
+        <div class="w-60 h-60 flex items-center justify-center p-8 rounded-full bg-white shadow-xl">
+          <img src="../assets//labeaker.png" alt="labratoar beaker" class="h-4/5" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
