546664805aa49cb0a099d2cdfcffc360b38c7df4
nightcode-dev <prog.nightcode@gmail.com>
2023-07-14 20:54:53 +0430

    makeing footer

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
   <div class="">
     <div class="fixed top-0 h-min flex justify-between p-2 w-full z-50 bg-gray-200 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border-b border-gray-400">
       <div class="flex items-center p-4 z-50 ">
-        <router-link active-class="text-main" to="/" class="Bpey text-xl mx-2">خانه</router-link>
-        <router-link active-class="text-main" to="/aboutUs" class="Bpey text-xl mx-2">درباره ما</router-link>
+        <router-link active-class="text-main underline decoration-main decoration-4 underline-offset-8" to="/" class="Bpey text-xl mx-2">خانه</router-link>
+        <router-link active-class="text-main underline decoration-main decoration-4 underline-offset-8" to="/pricing" class="Bpey text-xl mx-2">تعرفه ها</router-link>
       </div>
       <div class="flex justify-end items-center ">
         <img src="./assets/dark_logo.png" alt="logo" class=" h-12">
